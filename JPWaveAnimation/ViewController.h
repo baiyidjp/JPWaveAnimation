ac70179f68162c0cc00d5809b508c10441150e2a
jp <jiangpeng@cecelive.com>
2018-06-06 16:50:35 +0800

    Initial Commit

---
+++ JPWaveAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JPWaveAnimation
+//
+//  Created by baiyi on 2018/6/6.
+//  Copyright © 2018年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JPWaveAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JPWaveAnimation
+//
+//  Created by baiyi on 2018/6/6.
+//  Copyright © 2018年 dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
